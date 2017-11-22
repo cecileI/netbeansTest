8bcf3d1dbe857a2fe654576f79dc5682882a5855
Cecile <cecile.iaegi@gmail.com>
2017-11-22 15:57:13 +0100

    Initialize commit of the Test project

---
+++ ProjetTest/src/projettest/FFDebugTest.java
@@ -0,0 +1,36 @@
+package projettest;
+
+import java.util.Scanner;
+
+public class FFDebugTest {
+
+    private int[] myArray;
+    private int myInt;
+    private boolean myBoolean;
+    private String myString;
+
+    public static void main(String[] args) {
+        FFDebugTest debug = new FFDebugTest(new int[3]);
+        FFDebugTest.getValue(debug.myArray);
+        System.out.println("La plus grande valeur est : "
+                + FFDebugCalc.findMax(debug.myArray));
+        
+        System.out.println("La plus petite valeur est : "
+                + FFDebugCalc.findMin(debug.myArray));
+    }
+
+    public FFDebugTest(int[] myArray) {
+        this.myArray = myArray;
+    }
+
+    public static void getValue(int[] array) {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Entrer un nombre : ");
+            array[i] = scan.nextInt();
+        }
+    }
+    
+
+   
+}
